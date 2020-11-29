b1c0c6f66787a3db510febd96eaadf5008c9cbdc
pigmon <dudgh5184@naver.com>
2020-11-29 23:38:11 +0900

    ~#3.12 검색엔진 설정 및 검색 창 변경, 동영상 세부정보에서 댓글 수 확인가능

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,9 +1,10 @@
 import routes from "../routes";
 import Video from "../models/Video";
 
+// 홈 화면
 export const home = async (req, res) => {
   try {
-    const videos = await Video.find({});
+    const videos = await Video.find({}).sort({ _id: -1 });
     res.render("home", { pageTitle: "Home", videos });
   } catch (error) {
     console.log(error);
@@ -11,15 +12,26 @@ export const home = async (req, res) => {
   }
 };
 
-export const search = (req, res) => {
+// 비디오 검색
+export const search = async (req, res) => {
   const {
     query: { term: searchingBy },
   } = req;
+  let videos = [];
+  try {
+    videos = await Video.find({
+      title: { $regex: searchingBy, $options: "i" },
+    });
+  } catch (error) {
+    console.log(error);
+  }
   res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
 
+// 비디오 업로드
 export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
+
 export const postUpload = async (req, res) => {
   const {
     body: { title, description },
@@ -33,6 +45,7 @@ export const postUpload = async (req, res) => {
   res.redirect(routes.videoDetail(newVideo.id));
 };
 
+// 비디오 세부정보
 export const videoDetail = async (req, res) => {
   const {
     params: { id },
@@ -45,7 +58,7 @@ export const videoDetail = async (req, res) => {
   }
 };
 
-// Edit Video
+// 비디오 수정
 export const getEditVideo = async (req, res) => {
   const {
     params: { id },
@@ -78,11 +91,8 @@ export const deleteVideo = async (req, res) => {
     params: { id },
   } = req;
   try {
-    console.log(id);
     await Video.findOneAndRemove({ _id: id });
-  } catch (error) {
-    console.log(error);
-  }
+  } catch (error) {}
   // 요놈이 밖으로 나온 이유는 성공하던 실패하던 무조건 홈으로 가서, 중복 작성이기도 하여서
   res.redirect(routes.home);
 };
